76168bf6b62a96a6f2e94878097730d8a424ed5c
jmgalindor9802 <jmgalindor9802@gmail.com>
2024-06-17 19:09:38 -0500

    git ignore y connection string del context

--- icbf_app/Models/IcbfAppContext.cs
+++ icbf_app/Models/IcbfAppContext.cs
@@ -38,8 +38,8 @@ public partial class IcbfAppContext : DbContext
     public virtual DbSet<RegistroAvanceAcademico> RegistrosAvanceAcademicos { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-HP\\SQLEXPRESS;Initial Catalog=icbf_app;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+        => optionsBuilder.UseSqlServer("name=ConnectionStrings:DefaultConnection");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
