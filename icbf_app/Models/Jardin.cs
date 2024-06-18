28486058f6c83c08335da84105fc7bd268f0ed61
jmgalindor9802 <jmgalindor9802@gmail.com>
2024-06-18 16:11:55 -0500

    CRUD JARDINES COMPLETO

--- icbf_app/Models/Jardin.cs
+++ icbf_app/Models/Jardin.cs
@@ -10,12 +10,15 @@ public partial class Jardin
     [Required(ErrorMessage = "Campo obligatorio")]
     [MaxLength(100, ErrorMessage = "El nombre del jardin como maximo debetener 100 caracteres")]
     [MinLength(8, ErrorMessage = "El nombre del jardin como minimo debe tener 8 caracteres")]
+    [Display(Name = "Nombre")]
     public string NombreJardin { get; set; } = null!;
     [Required(ErrorMessage = "Campo obligatorio")]
     [MaxLength(200, ErrorMessage = "La direccion del jardin como maximo debetener 200 caracteres")]
     [MinLength(8, ErrorMessage = "La direccion del jardin como minimo debe tener 8 caracteres")]
+    [Display(Name = "Dirección")]
     public string DireccionJardin { get; set; } = null!;
     [Required(ErrorMessage = "Campo obligatorio")]
+    [Display(Name = "Estado")]
     public string EstadoJardin { get; set; } = null!;
 
     public virtual ICollection<MadreComunitaria> MadresComunitaria { get; set; } = new List<MadreComunitaria>();
