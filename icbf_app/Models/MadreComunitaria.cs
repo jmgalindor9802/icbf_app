305b647ae109b8fc4e5fa04bf86124f8bf0cea50
Luis Nicolas Arevalo Chiquiza <nicolasarevalochiquiza@gmail.com>
2024-06-17 20:52:46 -0500

    se agrego las rutas de los controller al menu de navegacion, se agrego algunas validaciones a los entitys

--- icbf_app/Models/MadreComunitaria.cs
+++ icbf_app/Models/MadreComunitaria.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace icbf_app.Models;
 
 public partial class MadreComunitaria
 {
     public int IdMadreComunitaria { get; set; }
+    [Required(ErrorMessage = "Campo obligatorio")]
 
     public DateOnly FechaNacimientoMadre { get; set; }
-
+    [Required(ErrorMessage = "Campo obligatorio")]
     public int IdJardin { get; set; }
-
+    [Required(ErrorMessage = "Campo obligatorio")]
     public string IdUsuario { get; set; } = null!;
 
     public virtual Jardin IdJardinNavigation { get; set; } = null!;
