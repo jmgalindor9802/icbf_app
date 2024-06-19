f90bdc702f07088a690a1baa30c9cb9510871c85
Luis Nicolas Arevalo Chiquiza <nicolasarevalochiquiza@gmail.com>
2024-06-18 19:42:41 -0500

    validaciones completas

--- icbf_app/Models/RegistroAsistencia.cs
+++ icbf_app/Models/RegistroAsistencia.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace icbf_app.Models;
 
 public partial class RegistroAsistencia
 {
+
     public int IdRegistroAsistencia { get; set; }
-
+    [Required(ErrorMessage = "El campo es obligatorio")]
     public long IdNino { get; set; }
-
+    [Required(ErrorMessage = "El campo es obligatorio")]
     public DateOnly FechaRegistro { get; set; }
-
+    [Required(ErrorMessage = "El campo es obligatorio")]
     public string EstadoNinoRegistro { get; set; } = null!;
-
     public virtual Nino IdNinoNavigation { get; set; } = null!;
 }
--- icbf_app/Models/RegistroAvanceAcademico.cs
+++ icbf_app/Models/RegistroAvanceAcademico.cs
@@ -1,23 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace icbf_app.Models;
 
 public partial class RegistroAvanceAcademico
 {
     public int IdAvance { get; set; }
-
+    [Required(ErrorMessage = "El campo es obligatorio")]
     public long IdNino { get; set; }
-
+    [Required(ErrorMessage = "El campo es obligatorio")]
     public int AnioEscolarAvance { get; set; }
-
+    [Required(ErrorMessage = "El campo es obligatorio")]
     public string NivelAvance { get; set; } = null!;
-
+    [Required(ErrorMessage = "El campo es obligatorio")]
     public string NotaAvance { get; set; } = null!;
-
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    [MaxLength(5000, ErrorMessage = "La descripcion debe tener maximo 5000 caracteres")]
+    [MinLength(10, ErrorMessage = "La descripcion debe tener minimo 10 caracteres")]
     public string DescripcionAvance { get; set; } = null!;
-
+    [Required(ErrorMessage = "El campo es obligatorio")]
     public DateOnly FechaEntregaAvance { get; set; }
-
     public virtual Nino IdNinoNavigation { get; set; } = null!;
 }
